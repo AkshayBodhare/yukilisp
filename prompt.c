84e244d8aeec6752f8332822f7fdc5ff47a8fe12
AkshayBodhare <abodhare1234@gmail.com>
2017-09-07 22:40:25 +0530

    Still need to add the negation feature

--- prompt.c
+++ prompt.c
@@ -27,6 +27,52 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long min(long x, long y) {
+    if (x > y) { return y; }
+    else { return x; }
+}
+
+long max(long x, long y) {
+    if (x > y) { return x; }
+    else { return y; }
+}
+
+/* Use operator string to see which operation to perform. */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
+    if (strcmp(op, "^") == 0) { return pow(x, y); }
+    if (strcmp(op, "min") == 0) { return min(x, y); }
+    if (strcmp(op, "max") == 0) { return max(x, y); }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    /* If tagged as number return it directly. */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child. */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in 'x' */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
@@ -38,7 +84,7 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
             "\
             number   : /-?[0-9]+(\\.[0-9]+)?/ ;\
-            operator : '+' | '-' | '*' | '/' | '%' \
+            operator : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\"\
                      | \"add\" | \"sub\" | \"mul\" | \"div\"; \
             expr     : <number> | '(' <operator> <expr>+ ')' ;\
             yuki     : /^/ <operator> <expr>+ /$/ ;\
@@ -46,7 +92,7 @@ int main(int argc, char** argv) {
             Number, Operator, Expr, Yuki);
 
     /* Print Version and Exit Information */
-    puts("YUKI Version 0.0.0.0.2");
+    puts("YUKI Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
@@ -61,15 +107,17 @@ int main(int argc, char** argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Yuki, &r)) {
-            /* On Success Print the AST */
-            mpc_ast_print(r.output);
+
+            /* Evalute the expressions */
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
+
         } else {
             /* Otherwise Print the Error */
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
         /* Free retrieved input */
         free(input);
     }
