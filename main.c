4636691fc5d169f9bdb2347fdd1efe6529fd447b
AkshayBodhare <abodhare1234@gmail.com>
2017-09-10 00:04:25 +0530

    Added Evaluation of S-expressions

--- main.c
+++ main.c
@@ -101,6 +101,29 @@ void lval_del(lval* v) {
     free(v);
 }
 
+/* Define the pop function for lval */
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at "i" */
+    lval* x = v->cell[i];
+
+    /* Shift memory after the item at "i" over the top */
+    memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+/* Single out an lval by it's index */
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
 /* Convert number from string to double */
 lval* lval_read_num(mpc_ast_t* t) {
     errno = 0;
@@ -171,6 +194,104 @@ void lval_expr_print(lval* v, char open, char close) {
 /* Print an "lval" followed by a newline */
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+/* Do all the computations here */
+lval* builtin_op(lval* a, char* op) {
+
+    /* Ensure all arguments are numbers */
+    for (int i = 0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and sub then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;
+    }
+
+    /* While there are still elements remaining */
+    while (a->count > 0) {
+
+        /* Pop the next element */
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "^") == 0) { x->num = pow(x->num, y->num); }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x); lval_del(y);
+                x = lval_err("Division By Zero!"); break;
+            }
+            x->num /= y->num;
+        }
+        if (strcmp(op, "%") == 0) {
+            if (y->num == 0) {
+                lval_del(x); lval_del(y);
+                x = lval_err("Modulo by Zero!"); break;
+            }
+            if (rint(x->num) == x->num && rint(y->num) == y-> num) {
+                x->num = (long)x->num % (long)y->num;
+            } else {
+                lval_del(x); lval_del(y);
+                x = lval_err("Modulo without integer operands"); break;
+            }
+        }
+        
+        lval_del(y);
+    }
+
+    lval_del(a); return x;
+}
+
+/* Declare lval_eval_sexpr */
+lval* lval_eval_sexpr(lval* v);
+
+/* Evaluate expressions by transforming lval */
+lval* lval_eval(lval* v) {
+    /* Evaluate S-expressions */
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    /* All other lval types remain the same */
+    return v;
+}
+
+/* Evaluate S-expressions */
+lval* lval_eval_sexpr(lval* v) {
+
+    /* Evaluate Children */
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Error Checking */
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    /* Empty Expression */
+    if (v->count == 0) { return v; }
+
+    /* Single Expression */
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    /* Ensure First Element is Symbol */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f); lval_del(v);
+        return lval_err("S-expression does not start with a symbol!");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
 int main(int argc, char** argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
@@ -208,8 +329,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Yuki, &r)) {
 
-            /* Simply print the input */
-            lval* x = lval_read(r.output);
+            /* Evaluate and print the input */
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
             mpc_ast_delete(r.output);
