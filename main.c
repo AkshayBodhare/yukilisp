c77900cd52a640c0f3c58ae9c36a62df119393fd
AkshayBodhare <abodhare1234@gmail.com>
2017-09-09 19:04:34 +0530

    Changed internal representation of lval

--- main.c
+++ main.c
@@ -28,59 +28,148 @@ void add_history(char* unused) {}
 #endif
 
 /* Create Enumeration of Possible lval Types */
-enum { LVAL_NUM, LVAL_ERR };
-
-/* Create Enumeration of Possible Error Types */
-enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 
 /* Declare New lval Struct */
-typedef struct {
+typedef struct lval {
     int type;
     double num;
-    int err;
+    /* Error and Symbol types have some string data */
+    char* err;
+    char* sym;
+    /* Count and Pointer to a list of "lval*" */
+    int count;
+    struct lval** cell;
 } lval;
 
 /* Create a new number type lval */
-lval lval_num(double x) {
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+lval* lval_num(double x) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
 /* Create a new error type lval */
-lval lval_err(int x) {
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+lval* lval_err(char* m) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m) + 1);
+    strcpy(v->err, m);
     return v;
 }
 
-/* Print an "lval" */
-void lval_print(lval v) {
-    switch (v.type) {
-        /* In the case the type is a number print it */
-        /* Then 'break' out of the switch. */
-        case LVAL_NUM: printf("%g", v.num);break;
+/* Construct a pointer to a new Symbol lval */
+lval* lval_sym(char* s) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(strlen(s) + 1);
+    strcpy(v->sym, s);
+    return v;
+}
 
-        /* In the case the type is an error */
-        case LVAL_ERR:
-        /* Check what type of error it is and print it */
-            if (v.err == LERR_DIV_ZERO) {
-               printf("Error: Division By Zero!");
+/* A pointer to a new empty Sexpr lval */
+lval* lval_sexpr(void) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
+/* Destructor for lval */
+void lval_del(lval* v) {
+    switch (v->type) {
+        /* Do nothing special for number type */
+        case LVAL_NUM: break;
+
+        /* For Err or Sym free the string data */
+        case LVAL_ERR: free(v->err); break;
+        case LVAL_SYM: free(v->sym); break;
+
+        /* If Sexpr then delete all elements inside */
+        case LVAL_SEXPR:
+            for (int i = 0; i < v->count; i++) {
+               lval_del(v->cell[i]);
             }
-            if (v.err == LERR_BAD_OP) {
-               printf("Error: Invalid Operator!");
-            }
-            if (v.err == LERR_BAD_NUM) {
-               printf("Error: Invlaid Number!");
-            }
+            /* Also free the memory allocated to contain the pointers */
+            free(v->cell);
             break;
+    }
+
+    /* Free the memory allocated for the "lval" struct itself */
+    free(v);
+}
+
+/* Convert number from string to double */
+lval* lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    double x = strtod(t->contents, NULL);
+    return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+/* Add another lval value to the cell field */
+lval* lval_add(lval* v, lval* x) {
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    v->cell[v->count-1] = x;
+    return v;
+}
+
+/* Convert parsed tree structure completely into an lval */
+lval* lval_read(mpc_ast_t* t) {
+
+    /* If Symbol or Number return conversion to that type */
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    /* If root (>) or sexpr then create empty list */
+    lval* x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strcmp(t->tag, "sexpr")) { x = lval_sexpr(); }
+
+    /* Fill this list with any valid expression contained within */
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
+}
+
+/* Declaration for lval_expr_print */
+void lval_expr_print(lval* v, char open, char close);
+
+/* Print the lval as it is */
+void lval_print(lval* v) {
+    switch (v->type) {
+        case LVAL_NUM: printf("%g", v->num); break;
+        case LVAL_ERR: printf("Error: %s", v->err); break;
+        case LVAL_SYM: printf("%s", v->sym); break;
+        case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
     }
 }
 
-/* Print and "lval" followed by a newline */
-void lval_println(lval v) { lval_print(v); putchar('\n'); }
+/* Print expressions */
+void lval_expr_print(lval* v, char open, char close) {
+    putchar(open);
+    for (int i = 0; i < v->count; i++) {
+
+        /* Print Value contained within */
+        lval_print(v->cell[i]);
+
+        /* Don't print trailing space if last element */
+        if (i != (v->count-1)) {
+            putchar(' ');
+        }
+    }
+    putchar(close);
+}
+
+/* Print an "lval" followed by a newline */
+void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 
 /* Use operator string to see which operation to perform. */
@@ -150,7 +239,8 @@ lval eval(mpc_ast_t* t) {
 int main(int argc, char** argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Yuki = mpc_new("yuki");
 
@@ -158,12 +248,13 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
             "\
             number   : /-?[0-9]+(\\.[0-9]+)?/ ;\
-            operator : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\"\
+            symbol : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\"\
                      | \"add\" | \"sub\" | \"mul\" | \"div\"; \
-            expr     : <number> | '(' <operator> <expr>+ ')' ;\
+            sexpr : '(' <expr>* ')' ; \
+            expr : <number> | <symbol> | <sexpr> ; \
             yuki     : /^/ <operator> <expr>+ /$/ ;\
             ",
-            Number, Operator, Expr, Yuki);
+            Number, Symbol, Sexpr, Expr, Yuki);
 
     /* Print Version and Exit Information */
     puts("YUKI Version 0.0.0.0.3");
@@ -182,9 +273,10 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Yuki, &r)) {
 
-            /* Evalute the expressions */
-            lval result = eval(r.output);
-            lval_println(result);
+            /* Simply print the input */
+            lval* x = lval_read(r.output);
+            lval_println(x);
+            lval_del(x);
             mpc_ast_delete(r.output);
 
         } else {
@@ -197,7 +289,7 @@ int main(int argc, char** argv) {
     }
 
     /* Undefine and Delete our Parsers */
-    mpc_cleanup(4, Number, Operator, Expr, Yuki);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Yuki);
 
     return 0;
 }
