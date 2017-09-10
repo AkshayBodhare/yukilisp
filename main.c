aa6aac4698612bb5121d730f83dc8c0a0cf1f7d2
AkshayBodhare <abodhare1234@gmail.com>
2017-09-10 14:21:06 +0530

    Added evaluation and builtin functions for Q-expressions

--- main.c
+++ main.c
@@ -27,6 +27,9 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+#define LASSERT(args, cond, err) \
+    if (!(cond)) { lval_del(args); return lval_err(err); }
+
 /* Create Enumeration of Possible lval Types */
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
@@ -263,6 +266,42 @@ lval* builtin_op(lval* a, char* op) {
     lval_del(a); return x;
 }
 
+/* The head function for Q-expressions */
+lval* builtin_head(lval* a) {
+    /* Check Error Conditions */
+    LASSERT(a, a->count == 1,
+            "Function 'head' too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+            "Function 'head' passed incorrect type!");
+    LASSERT(a, a->cell[0]->count != 0,
+            "Function 'head' passed {}!");
+
+    /* Otherwise take first argument */
+    lval* v = lval_take(a, 0);
+
+    /* Delete all elements that are not head and return */
+    while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+    return v;
+}
+
+/* Builtin tail function for Q-expressions */
+lval* builtin_tail(lval* a) {
+    /* Check Error Conditions */
+    LASSERT(a, a->count == 1,
+            "Function 'tail' passed too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+            "Function 'tail' passed incorrect type!");
+    LASSERT(a, a->cell[0]->count != 0,
+            "Function 'tail' passed {}!");
+
+    /* Take first argument */
+    lval* v = lval_take(a, 0);
+
+    /* Delete first element and return */
+    lval_del(lval_pop(v, 0));
+    return v;
+}
+
 /* Declare lval_eval_sexpr */
 lval* lval_eval_sexpr(lval* v);
 
@@ -273,6 +312,9 @@ lval* lval_eval(lval* v) {
     /* All other lval types remain the same */
     return v;
 }
+
+/* Declare builtin function */
+lval* builtin(lval* a, char* func);
 
 /* Evaluate S-expressions */
 lval* lval_eval_sexpr(lval* v) {
@@ -301,9 +343,71 @@ lval* lval_eval_sexpr(lval* v) {
     }
 
     /* Call builtin with operator */
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
+}
+
+/* Convert a S-expression to an Q-expression */
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+/* Convert a Q-expression to an S-expression and evaluate it */
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count == 1,
+            "Function 'eval' passed too many arguments!");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+            "Function 'eval' passed incorrect type!");
+
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+/* Function to join lval */
+lval* lval_join(lval* x, lval* y) {
+
+    /* For each cell in 'y' add it to 'x' */
+    while (y->count) {
+        x = lval_add(x, lval_pop(y, 0));
+    }
+
+    /* Delete the empty 'y' and return 'x' */
+    lval_del(y);
+    return x;
+}
+
+/* Join Q-expressions together */
+lval* builtin_join(lval* a) {
+
+    for (int i = 0; i < a ->count; i++) {
+        LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+                "Function 'join' passed incorrect type.");
+    }
+
+    lval* x = lval_pop(a, 0);
+
+    while (a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(a);
+    return x;
+}
+
+/* Function to evaluate expressions based on symbol */
+lval* builtin(lval* a, char* func) {
+   if (strcmp("list", func) == 0) { return builtin_list(a); }
+   if (strcmp("head", func) == 0) { return builtin_head(a); }
+   if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+   if (strcmp("join", func) == 0) { return builtin_join(a); }
+   if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+   if (strstr("+-/*addmuldivminmaxsub^%",
+               func)) { return builtin_op(a, func); }
+   lval_del(a);
+   return lval_err("Unknown Function!");
 }
 
 int main(int argc, char** argv) {
@@ -320,7 +424,9 @@ int main(int argc, char** argv) {
             "\
             number   : /-?[0-9]+(\\.[0-9]+)?/ ;\
             symbol : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\"\
-                     | \"add\" | \"sub\" | \"mul\" | \"div\"; \
+                     | \"add\" | \"sub\" | \"mul\" | \"div\" \
+                     | \"list\" | \"head\" | \"tail\" \
+                     | \"join\" | \"eval\"; \
             sexpr : '(' <expr>* ')' ; \
             qexpr : '{' <expr>* '}' ; \
             expr : <number> | <symbol> | <sexpr> | <qexpr> ; \
@@ -329,7 +435,7 @@ int main(int argc, char** argv) {
             Number, Symbol, Sexpr, Qexpr, Expr, Yuki);
 
     /* Print Version and Exit Information */
-    puts("YUKI Version 0.0.0.0.3");
+    puts("YUKI Version 0.0.0.0.4");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
